427ff47d507232f11e27b48faa4a67d5f496b4d1
unknown <majid.asadollahi95@gmail.com>
2024-01-20 22:15:48 +0330

    develop banner and home page

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -75,6 +75,21 @@ const NavItems = () => {
               <Link to="/login" className="d-none d-md-block">
                 Log In
               </Link>
+
+              {/* menu toggler */}
+              <div
+                onClick={() => setMenuToggle(!menuToggle)}
+                className={`header-bar d-lg-none ${menuToggle ? "active" : ""}`}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+
+              {/* social toggler */}
+              <div className="ellepsis-bar d-md-none" onClick={() => setSocialToggle(!socialToggle)}>
+                <i className="icofont-info-square"></i>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/home/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const title = (
+    <h2>Search Your One From Thousand of Products</h2>
+)
+
+const desc = "We have largest collection of products"
+
+const Banner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Banner
