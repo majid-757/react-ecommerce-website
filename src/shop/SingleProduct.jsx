e4c3b5c29a680b569bd299ea9fea2e998f0b5d2b
unknown <majid.asadollahi95@gmail.com>
2024-02-13 16:34:58 +0330

    develop product detail

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
 
 const SingleProduct = () => {
-  return (
-    <div>SingleProduct</div>
-  )
-}
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+  // console.log(id)
 
-export default SingleProduct
+  useEffect(() => {
+    fetch("/src/products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  const result = product.filter((p) => p.id === id);
+  console.log(result);
+
+  return (
+    <div>
+      <PageHeader title={"OUR SHOP SINGLE"} curPage={"Shop / Single Product"} />
+
+      <div className="shop-single padding-tb aside-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top"></div>
+                      </div>
+                    </div>
+                    <div className="col-md-6 col-12">Details</div>
+                  </div>
+                </div>
+
+                {/* reviews */}
+                <div className="review">Review</div>
+              </article>
+            </div>
+
+            {/* Right side */}
+            <div className="col-lg-4 col-12">Right Side</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
