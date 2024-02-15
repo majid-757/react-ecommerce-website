ee82a619beec5cfbcf9f7e0b8efd2d169bd63018
unknown <majid.asadollahi95@gmail.com>
2024-02-15 16:46:21 +0330

    develop cart

--- src/shop/ProductDispley.jsx
+++ src/shop/ProductDispley.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const desc =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nostrum dolores quo optio iste amet quae dolorem ipsum molestias soluta deleniti earum culpa maxime possimus quidem architecto facilis, placeat id!";
 
 const ProductDispley = ({ item }) => {
-  console.log(item);
+  // console.log(item);
 
-  const { name, id, price, seller, ratingsCount, quantity } = item;
+  const { name, id, price, seller, ratingsCount, quantity, img } = item;
 
   const [prequantity, setQuantity] = useState(quantity);
   const [coupon, setCoupon] = useState("");
@@ -19,6 +20,52 @@ const ProductDispley = ({ item }) => {
 
   const handleColorChange = (e) => {
     setColor(e.target.value);
+  };
+
+  const handleDecrease = () => {
+    if (prequantity > 1) {
+      setQuantity(prequantity - 1);
+    }
+  };
+
+  const handleIncrease = () => {
+    setQuantity(prequantity + 1);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const product = {
+      id: id,
+      img: img,
+      name: name,
+      price: price,
+      quantity: prequantity,
+      size: size,
+      color: color,
+      coupon: coupon,
+    };
+
+    // console.log(product)
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existingProductIndex = existingCart.findIndex(
+      (item) => item.id === id
+    );
+
+    if (existingProductIndex !== -1) {
+      existingCart[existingProductIndex].quantity += prequantity;
+    } else {
+      existingCart.push(product);
+    }
+
+    // update local storage
+    localStorage.setItem("cart", JSON.stringify(existingCart));
+
+    // reset form fields
+    setQuantity(1)
+    setSize("Select Size")
+    setColor("Select Color")
+
   };
 
   return (
@@ -40,7 +87,7 @@ const ProductDispley = ({ item }) => {
 
       {/* cart components */}
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="select-product size">
             <select value={size} onChange={handleSizeChange}>
               <option>Select Size</option>
@@ -52,6 +99,9 @@ const ProductDispley = ({ item }) => {
             </select>
             <i className="icofont-rounded-down"></i>
           </div>
+
+          {/* color */}
+
           <div className="select-product color">
             <select value={color} onChange={handleColorChange}>
               <option>Select Color</option>
@@ -64,6 +114,41 @@ const ProductDispley = ({ item }) => {
             </select>
             <i className="icofont-rounded-down"></i>
           </div>
+
+          {/* cart plus minus */}
+          <div className="cart-plus-minus">
+            <div className="dec qtybutton" onClick={handleDecrease}>
+              -
+            </div>
+            <input
+              className="cart-plus-minus-box"
+              type="text"
+              name="qtybutton"
+              id="qtybutton"
+              value={prequantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+            />
+            <div className="inc qtybutton" onClick={handleIncrease}>
+              +
+            </div>
+          </div>
+
+          {/* coupon field */}
+          <div className="discount-code mb-2">
+            <input
+              type="text"
+              placeholder="Enter Discount Code"
+              onChange={(e) => setCoupon(e.target.value)}
+            />
+          </div>
+
+          {/* btn section */}
+          <button type="submit" className="lab-btn">
+            <span>Add To Cart</span>
+          </button>
+          <Link to="/cart-page" className="lab-btn bg-primary">
+            <span>Check Out</span>
+          </Link>
         </form>
       </div>
     </div>
