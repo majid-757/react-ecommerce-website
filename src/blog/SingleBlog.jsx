6508525e4c31cc801f034ca037db2954216d4ed3
unknown <majid.asadollahi95@gmail.com>
2024-02-21 16:28:53 +0330

    develop blog

--- src/blog/SingleBlog.jsx
+++ src/blog/SingleBlog.jsx
@@ -37,17 +37,76 @@ const SingleBlog = () => {
                               <div className="post-content">
                                 <h2>{item.title}</h2>
                                 <div className="meta-post">
-
-                                    <ul className="lab-ul">
-                                      {item.metaList.map((val, i) => (
-                                        <li key={i}>
-                                          <i className={val.iconName}></i>
-                                          {val.text}
-                                        </li>
-                                      ))}
-                                    </ul>
-                                
+                                  <ul className="lab-ul">
+                                    {item.metaList.map((val, i) => (
+                                      <li key={i}>
+                                        <i className={val.iconName}></i>
+                                        {val.text}
+                                      </li>
+                                    ))}
+                                  </ul>
                                 </div>
+                                <p>
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Ab eos asperiores
+                                  reprehenderit accusantium alias ducimus.
+                                  Blanditiis sapiente ullam veniam sint! Iure in
+                                  quae, aspernatur facilis voluptatum quo
+                                  impedit exercitationem voluptates.
+                                </p>
+                                <blockquote>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur,
+                                    adipisicing elit. Dolore rem nisi molestiae
+                                    culpa, natus distinctio, explicabo harum
+                                    beatae commodi eos, sequi asperiores porro
+                                    consequuntur nesciunt impedit nam
+                                    dignissimos fugit eveniet.
+                                  </p>
+                                  <cite>
+                                    <a href="#">...Mellissa Hunter</a>
+                                  </cite>
+                                </blockquote>
+
+                                <p>
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Modi aliquid laborum quidem
+                                  consectetur accusamus blanditiis debitis
+                                  deserunt adipisci harum nam ipsum, iste odit
+                                  cum, minus eaque ea quisquam autem ullam.
+                                </p>
+                                <img
+                                  src="/src/assets/images/blog/single/01.jpg"
+                                  alt=""
+                                />
+
+                                <p>
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Quasi vitae ab minima
+                                  temporibus. Ipsam nobis ab cum, accusantium
+                                  eum placeat temporibus consectetur sit,
+                                  dolorum, nisi tempore qui quibusdam et quo.
+                                </p>
+
+                                <div className="video-thumb">
+                                  <img
+                                    src="/src/assets/images/blog/single/02.jpg"
+                                    alt=""
+                                  />
+                                  <a
+                                    href="https://www.aparat.com/v/FyYhP"
+                                    className="video-button popup"
+                                  >
+                                    <i className="icofont-ui-play"></i>
+                                  </a>
+                                </div>
+                                <p>
+                                  Lorem ipsum dolor sit, amet consectetur
+                                  adipisicing elit. Nam, harum! Ex voluptatem
+                                  reiciendis nulla incidunt minus! Dolores nihil
+                                  ea totam natus amet dignissimos soluta harum
+                                  impedit dolor, vel ab sequi.
+                                </p>
                               </div>
                             </div>
                           ))}
